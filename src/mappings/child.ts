97a0e523ad9b253eba56df77c932d58328be995c
mutahhirkhan <mutahhir199@gmail.com>
2023-02-15 19:24:46 +0500

    initial commit

---
+++ src/mappings/child.ts
@@ -0,0 +1,7 @@
+import { InitializeChild } from '../../generated/templates/Child/Child';
+
+
+export function handleInitializedChild(event: InitializeChild): void {
+  let entity = new 
+
+}
--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,12 +1,13 @@
-import { ChildGeneration as ChildGenerationEvent } from "../generated/Factory/Factory"
-import { ChildGeneration } from "../generated/schema"
+import { ChildGeneration as ChildGenerationEvent } from "../../generated/Factory/Factory"
+import { ChildGeneration } from "../../generated/schema"
+import {Child} from "./../../generated/templates"
 
 export function handleChildGeneration(event: ChildGenerationEvent): void {
   let entity = new ChildGeneration(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity._parentAddress = event.params._parentAddress
-  entity._childAddress = event.params._childAddress
+  entity.parentAddress = event.params._parentAddress
+  entity.childAddress = event.params._childAddress
   entity.childNumber = event.params.childNumber
 
   entity.blockNumber = event.block.number
@@ -14,4 +15,7 @@ export function handleChildGeneration(event: ChildGenerationEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  Child.create(event.params._childAddress)
+
 }
