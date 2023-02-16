43f6486631793e3b16a5d52f5d8ad0fb990e1f71
mutahhirkhan <mutahhir199@gmail.com>
2023-02-16 18:35:20 +0500

    child entity initialzed issue fixed

---
+++ README.md
@@ -0,0 +1,1 @@
+sg deployed on goerli link: https://api.studio.thegraph.com/query/42216/template-method/4
--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,8 +1,13 @@
 import { ChildGeneration as ChildGenerationEvent } from "../../generated/Factory/Factory"
 import { ChildGeneration } from "../../generated/schema"
 import {Child} from "./../../generated/templates"
+import { handleChild } from '../core';
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleChildGeneration(event: ChildGenerationEvent): void {
+  log.info("CHILD GENERATION {}",[""])
+  handleChild(event.params._childAddress.toHexString())
+
   let entity = new ChildGeneration(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
@@ -16,6 +21,6 @@ export function handleChildGeneration(event: ChildGenerationEvent): void {
 
   entity.save()
 
-  Child.create(event.params._childAddress)
+  // Child.create(event.params._childAddress.toHexString())
 
 }
